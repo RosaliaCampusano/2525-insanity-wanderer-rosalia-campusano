3aca2d516d42ee2f677106b2c29ba8337d3edc97
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-10-23 09:08:13 +0200

    feat: create the structure of the project and implememnt the connection with MongoDB

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGO_CONNECTION = process.env.MONGO_CONNECTION;
+
+app.use(bodyParser.json());
+
+async function start() {
+  try {
+    await mongoose.connect(MONGO_CONNECTION);
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+    console.log("Connection with MongoDB successful");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+  }
+}
+
+start();
